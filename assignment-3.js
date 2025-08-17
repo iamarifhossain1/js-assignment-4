7c594c27fd3b4de0ec5c51f265b31bdbb1bd0d18
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-17 18:05:37 +0600

    assignment-complete

--- assignment-3.js
+++ assignment-3.js
@@ -56,4 +56,62 @@ function bestTeam(player1, player2) {
         return 'Tie'
     }
 }
+/** 
+ * Problem-04: Same Same But Different 
+ */
 
+function isSame(arr1 , arr2 ) {
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+        return 'Invalid'
+    }
+
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+/**
+ * Problem-05: Exam Result Report Generator
+ */
+
+function resultReport(marks) {
+    if (!Array.isArray(marks)) {
+        return 'Invalid'
+    }
+
+    if (marks.length === 0) {
+        return {finalScore: 0, pass: 0, fail: 0};
+    }
+
+    let total = 0;
+    let passCount = 0;
+    let failCount = 0;
+    
+    for (let mark of marks) {
+        total = total + mark;
+        if (mark >= 40) {
+            passCount++
+        }
+
+        else { 
+            failCount++;
+        }
+    }
+
+    const avg = Math.round(total / marks.length);
+
+    return { 
+        finalScore: avg,
+        pass: passCount,
+        fail: failCount
+    };
+}
